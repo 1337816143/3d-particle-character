513b13461c7a82325846cf7a284a3bec3a004eda
Foam <1337816143@qq.com>
2026-02-09 09:58:00 +0800

    实现金色主题优化：球形、心形、文字粒子效果

---
+++ gold-theme-texts.js
@@ -0,0 +1,16 @@
+const GOLD_THEME_TEXTS = [
+    "我爱你",
+    "永远爱你",
+    "你是我的唯一",
+    "我的挚爱",
+    "一生一世"
+];
+
+const GOLD_THEME_CONFIG = {
+    textDisplayDuration: 4000,
+    font: "Source Han Sans, Microsoft YaHei, sans-serif",
+    fontSize: 200,
+    canvasWidth: 2000,
+    canvasHeight: 500,
+    particleDensity: 0.8
+};
